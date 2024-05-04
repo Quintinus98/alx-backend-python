c88dcc88b15515d5f4bf9c26c1eb4150f8560084
David <obododavidsop@gmail.com>
2024-05-04 13:44:58 +0100

    added floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Floor
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of the float
+    """
+    return round(n // 1)
+
+
+if __name__ == "__main__":
+    floor()
